672e8cda0acef67ce6af2b183c59fe6ac0d6b48b
PAU45 <paulo.melendez@tecsup.edu.pe>
2025-05-08 05:54:22 -0500

    base de datos

--- config/db.config.js
+++ config/db.config.js
@@ -1,15 +1,14 @@
 export default {
-    HOST: "localhost",
-    USER: "root",
-    PASSWORD: "",
-    DB: "db",
-    PORT: 3306,
-    dialect: "mysql",
-    pool: {
-      max: 5,
-      min: 0,
-      acquire: 30000,
-      idle: 10000
-    }
-  };
-  
+  HOST: "dpg-d0e8ov8dl3ps73bfrs40-a", // Host proporcionado por Render
+  USER: "root", // Usuario proporcionado
+  PASSWORD: "cJR3CbedVaqOAywDFDfdOkq5OxeRce9K", // Contraseña proporcionada
+  DB: "db_umhz", // Nombre de la base de datos
+  dialect: "postgres", // Dialecto para PostgreSQL
+  PORT: 5432, // Puerto proporcionado
+  pool: {
+    max: 5, // Número máximo de conexiones en el pool
+    min: 0, // Número mínimo de conexiones en el pool
+    acquire: 30000, // Tiempo máximo en ms para intentar conectar antes de lanzar un error
+    idle: 10000 // Tiempo máximo en ms que una conexión puede estar inactiva antes de ser liberada
+  }
+};
